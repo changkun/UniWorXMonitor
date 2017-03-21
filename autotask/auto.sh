3caf9a705a7365e454da1c68832dabf6a8c1572a
Changkun Ou <hi@changkun.us>
2017-03-21 16:32:37 +0100

    (fix): improve auto.sh notification

--- autotask/auto.sh
+++ autotask/auto.sh
@@ -1,9 +1,18 @@
-cd /PATH/TO/YOUR/UniWorxMonitor
+# uniworx=/Users/changkun/Documents/Git/UniWorxMonitor
+uniworx=/PATH/TO/YOUR/UniworXMonitor
+cd $uniworx
+found=false
 
-if python main.py | grep new; then
-    message="New Course Found! Check log.txt for details..."
+if python main.py | grep -q new; then
+    message="New Course Found! Check 'log.txt' for details..."
+    found=true
 else
     message="No Further Changes."
 fi
 
 osascript -e "display notification \" $message \" with title \"UniWorXMonitor\""
+
+echo $found
+if "$found" = true; then
+    osascript -e "tell application \"Finder\" to open POSIX file \"$uniworx/log.txt\""
+fi
